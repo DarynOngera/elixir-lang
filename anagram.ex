dad9ad6c6c617e322d90154560ffb06b957a822e
Daryn Ongera <ongeradaryn@gmail.com>
2026-01-18 14:51:19 +0300

    anagram checker

--- anagram.ex
+++ anagram.ex
@@ -1,8 +1,15 @@
 defmodule Anagram do
   def anagram() do
-    
+  a = IO.gets("First word: ") 
+  b = IO.gets("Second word: ")
+  sort_string(a) == sort_string(b)
   end
 
-  def sort_string() do
+  def sort_string(string) do
+    string
+    |> String.trim()
+    |> String.downcase()
+    |> String.graphemes()
+    |> Enum.sort()
   end 
 end
