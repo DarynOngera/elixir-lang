6d5e0fc3f74d7c3936af8fdd46a37e44afc6e50d
Ongera <ongeradaryn@gmail.com>
2026-01-21 00:52:32 +0300

    chore: update user schema

--- lib/crud/user.ex
+++ lib/crud/user.ex
@@ -16,6 +16,7 @@ defmodule Crud.User do
     |> validate_required([:name, :age])
     |> validate_format(:email, ~r/@/)
     |> validate_inclusion(:age, 18..100)
+    |> validate_number(:age, greater_than_or_equal_to: 18)
     |> unique_constraint(:email)
   end
 end
